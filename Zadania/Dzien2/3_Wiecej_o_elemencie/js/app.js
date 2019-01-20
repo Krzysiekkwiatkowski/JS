bdd9c12e701e9f4f1148a75225b27c9157ab539a
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2019-01-20 21:22:39 +0100

    Done some exercises from elements

--- Zadania/Dzien2/3_Wiecej_o_elemencie/js/app.js
+++ Zadania/Dzien2/3_Wiecej_o_elemencie/js/app.js
@@ -5,13 +5,41 @@ document.addEventListener("DOMContentLoaded", function() {
     var lis = document.querySelectorAll("ul > li");
     for (var li of lis){
         if(!li.hasAttribute("data-direction")){
-            li.dataset.derection = "up";
+            li.dataset.direction = "up";
         }
     }
 
     for (var i = 0; i < lis.length; i++) {
-        if(lis[i] % 2 != 0){
-            lis[i].setAttribute("backgrounColor", "green");
+        if(i % 2 != 0){
+            lis[i].style.backgroundColor = "green";
         }
     }
+
+    lis[4].setAttribute("class", "big");
+
+    for (var i = 0; i < lis.length; i++) {
+        if(i % 3 == 0){
+            lis[i].style.textDecoration = "underline";
+        }
+    }
+
+    //Zadanie 2
+    var option = document.querySelectorAll("select > option");
+    for (var i = 0; i < option.length; i++) {
+        option[i].innerText = option[i].getAttribute("value");
+    }
+
+    for (var i = 0; i < option.length; i++) {
+        option[i].dataset.year = parseInt(option[i].getAttribute("value")) + 20;
+    }
+
+    //Zadanie 3
+    var firefox = document.querySelector(".firefox");
+    firefox.style.backgroundImage = "url(\"assets/img/firefox.png\")";
+
+    var edge = document.querySelector(".edge");
+    edge.style.backgroundImage = "url(\"assets/img/edge.png\")";
+
+    var chrome = document.querySelector(".chrome");
+    chrome.style.width = "100px";
 });
